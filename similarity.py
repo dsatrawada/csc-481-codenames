a4f12b54a3f3305ffc26a4aa2a92257ec1d86c04
rkthomps <rkthomps@calpoly.edu>
2022-05-26 16:33:34 -0700

    Add similarity encapsulation

---
+++ similarity.py
@@ -0,0 +1,38 @@
+import sys, os
+
+import json
+from cache import CacheWriter
+
+
+class Similarity():
+    def __init__(self):
+        '''
+        Given two words, this class provides means to find the similarity between
+        them 
+        '''
+        pass
+
+    def similarity(self, w1: str, w2: str)  -> float:
+        '''
+        Returns the Hypernym Similarity between word1 and
+        word2. 
+        :param w1: first word of the pair.
+        :param w2: second word of the pair.
+        :returns a float representing the similarity between 
+            the two words
+        '''
+        return 0.1
+    
+    def gen_id(self, w1: str, w2: str) -> str:
+        '''
+        Get a string id for the two words based on the function
+        implemented by CacheWriter
+        :param w1: Fisrt word
+        :param w2: Second word
+        :returns: string representing the id
+        '''
+        return CacheWriter.gen_id(w1, w2)
+
+
+
+
